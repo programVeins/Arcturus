d499b06fa62425537f56da063fcff0e03a5aa818
Sabesh Bharathi <sabesh5601@gmail.com>
2020-07-05 12:16:21 +0530

    Nav icons 2

--- src/Components/NavComp.js
+++ src/Components/NavComp.js
@@ -70,45 +70,100 @@ export default class NavComp extends Component {
         return (
                 <>
                     <div className="row justify-content-center align-items-center">
-                        <div className="col">
-                            <div className={this.state.iconCol + " icorotate"} onClick={this.handleTheme}
+
+                        {/* Large Nav Icons */}
+                        <div className="col d-none d-md-block">
+                            <div className={this.state.iconCol + " icorotate largenav"} onClick={this.handleTheme}
                                 onMouseEnter={this.showtheme} onMouseLeave={this.hidetheme}>
                                 {this.state.iconCol === "icolight" ?
-                                    <svg class="bi bi-sun" width="2em" height="2em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                                    <svg class="bi bi-sun" width="100%" height="auto" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
                                         <path d="M3.5 8a4.5 4.5 0 1 1 9 0 4.5 4.5 0 0 1-9 0z"/>
                                         <path fill-rule="evenodd" d="M8.202.28a.25.25 0 0 0-.404 0l-.91 1.255a.25.25 0 0 1-.334.067L5.232.79a.25.25 0 0 0-.374.155l-.36 1.508a.25.25 0 0 1-.282.19l-1.532-.245a.25.25 0 0 0-.286.286l.244 1.532a.25.25 0 0 1-.189.282l-1.509.36a.25.25 0 0 0-.154.374l.812 1.322a.25.25 0 0 1-.067.333l-1.256.91a.25.25 0 0 0 0 .405l1.256.91a.25.25 0 0 1 .067.334L.79 10.768a.25.25 0 0 0 .154.374l1.51.36a.25.25 0 0 1 .188.282l-.244 1.532a.25.25 0 0 0 .286.286l1.532-.244a.25.25 0 0 1 .282.189l.36 1.508a.25.25 0 0 0 .374.155l1.322-.812a.25.25 0 0 1 .333.067l.91 1.256a.25.25 0 0 0 .405 0l.91-1.256a.25.25 0 0 1 .334-.067l1.322.812a.25.25 0 0 0 .374-.155l.36-1.508a.25.25 0 0 1 .282-.19l1.532.245a.25.25 0 0 0 .286-.286l-.244-1.532a.25.25 0 0 1 .189-.282l1.508-.36a.25.25 0 0 0 .155-.374l-.812-1.322a.25.25 0 0 1 .067-.333l1.256-.91a.25.25 0 0 0 0-.405l-1.256-.91a.25.25 0 0 1-.067-.334l.812-1.322a.25.25 0 0 0-.155-.374l-1.508-.36a.25.25 0 0 1-.19-.282l.245-1.532a.25.25 0 0 0-.286-.286l-1.532.244a.25.25 0 0 1-.282-.189l-.36-1.508a.25.25 0 0 0-.374-.155l-1.322.812a.25.25 0 0 1-.333-.067L8.203.28zM8 2.5a5.5 5.5 0 1 0 0 11 5.5 5.5 0 0 0 0-11z"/>
                                     </svg> :
-                                    <svg class="bi bi-moon" width="2em" height="2em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                                    <svg class="bi bi-moon" width="100%" height="auto" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
                                         <path fill-rule="evenodd" d="M14.53 10.53a7 7 0 0 1-9.058-9.058A7.003 7.003 0 0 0 8 15a7.002 7.002 0 0 0 6.53-4.47z"/>
                                     </svg> 
                                 }
                             </div>
                         </div>  
-                        <div className="col">
-                            <div className={this.state.iconCol + " icozoom"}
+                        <div className="col d-none d-md-block">
+                            <div className={this.state.iconCol + " icozoom largenav"}
                                 onMouseEnter={this.showhome} onMouseLeave={this.hidehome}>
                                 <NavLink className="nonedec" to="/home">
-                                    <svg width="1.9em" height="1.9em" viewBox="0 0 16 16" class="bi bi-hexagon-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                                    <svg width="100%" height="auto" viewBox="0 0 16 16" class="bi bi-hexagon-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
                                         <path fill-rule="evenodd" d="M8.5.134a1 1 0 0 0-1 0l-6 3.577a1 1 0 0 0-.5.866v6.846a1 1 0 0 0 .5.866l6 3.577a1 1 0 0 0 1 0l6-3.577a1 1 0 0 0 .5-.866V4.577a1 1 0 0 0-.5-.866L8.5.134z"/>
                                     </svg>
                                 </NavLink>
                             </div>
                         </div>  
-                        <div className="col">
-                            <div className={this.state.iconCol + " icozoom"}
+                        <div className="col d-none d-md-block">
+                            <div className={this.state.iconCol + " icozoom largenav"}
                                 onMouseEnter={this.showabout} onMouseLeave={this.hideabout}>
                                 <NavLink className="nonedec" to="/about">
-                                    <svg class="bi bi-person-fill" width="2.4em" height="2.4em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                                    <svg class="bi bi-person-fill" width="100%" height="auto" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
                                         <path fill-rule="evenodd" d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"/>
                                     </svg>
                                 </NavLink>
                             </div>
                         </div>
-                        <div className="col">
-                            <div className={this.state.iconCol + " icozoom"}
+                        <div className="col d-none d-md-block">
+                            <div className={this.state.iconCol + " icozoom largenav"}
                                 onMouseEnter={this.showblog} onMouseLeave={this.hideblog}>
                                 <NavLink className="nonedec" to="/blog">
-                                    <svg width="2em" height="2em" viewBox="0 0 16 16" class="bi bi-file-text-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                                    <svg width="100%" height="auto" viewBox="0 0 16 16" class="bi bi-file-text-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                                        <path fill-rule="evenodd" d="M12 1H4a2 2 0 0 0-2 2v10a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V3a2 2 0 0 0-2-2zM5 4a.5.5 0 0 0 0 1h6a.5.5 0 0 0 0-1H5zm-.5 2.5A.5.5 0 0 1 5 6h6a.5.5 0 0 1 0 1H5a.5.5 0 0 1-.5-.5zM5 8a.5.5 0 0 0 0 1h6a.5.5 0 0 0 0-1H5zm0 2a.5.5 0 0 0 0 1h3a.5.5 0 0 0 0-1H5z"/>
+                                    </svg>
+                                </NavLink>
+                            </div>
+                        </div>
+
+                        {/* Small Nav Icons */}
+                        <div className="col d-block d-md-none">
+                            <div className={this.state.iconCol + " icorotate smallnav"} onClick={() => {this.handleTheme(); this.showtheme(); setInterval(() => {
+                                this.hidetheme();
+                            }, 1000);}}>
+                                {this.state.iconCol === "icolight" ?
+                                    <svg class="bi bi-sun" width="100%" height="auto" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                                        <path d="M3.5 8a4.5 4.5 0 1 1 9 0 4.5 4.5 0 0 1-9 0z"/>
+                                        <path fill-rule="evenodd" d="M8.202.28a.25.25 0 0 0-.404 0l-.91 1.255a.25.25 0 0 1-.334.067L5.232.79a.25.25 0 0 0-.374.155l-.36 1.508a.25.25 0 0 1-.282.19l-1.532-.245a.25.25 0 0 0-.286.286l.244 1.532a.25.25 0 0 1-.189.282l-1.509.36a.25.25 0 0 0-.154.374l.812 1.322a.25.25 0 0 1-.067.333l-1.256.91a.25.25 0 0 0 0 .405l1.256.91a.25.25 0 0 1 .067.334L.79 10.768a.25.25 0 0 0 .154.374l1.51.36a.25.25 0 0 1 .188.282l-.244 1.532a.25.25 0 0 0 .286.286l1.532-.244a.25.25 0 0 1 .282.189l.36 1.508a.25.25 0 0 0 .374.155l1.322-.812a.25.25 0 0 1 .333.067l.91 1.256a.25.25 0 0 0 .405 0l.91-1.256a.25.25 0 0 1 .334-.067l1.322.812a.25.25 0 0 0 .374-.155l.36-1.508a.25.25 0 0 1 .282-.19l1.532.245a.25.25 0 0 0 .286-.286l-.244-1.532a.25.25 0 0 1 .189-.282l1.508-.36a.25.25 0 0 0 .155-.374l-.812-1.322a.25.25 0 0 1 .067-.333l1.256-.91a.25.25 0 0 0 0-.405l-1.256-.91a.25.25 0 0 1-.067-.334l.812-1.322a.25.25 0 0 0-.155-.374l-1.508-.36a.25.25 0 0 1-.19-.282l.245-1.532a.25.25 0 0 0-.286-.286l-1.532.244a.25.25 0 0 1-.282-.189l-.36-1.508a.25.25 0 0 0-.374-.155l-1.322.812a.25.25 0 0 1-.333-.067L8.203.28zM8 2.5a5.5 5.5 0 1 0 0 11 5.5 5.5 0 0 0 0-11z"/>
+                                    </svg> :
+                                    <svg class="bi bi-moon" width="100%" height="auto" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                                        <path fill-rule="evenodd" d="M14.53 10.53a7 7 0 0 1-9.058-9.058A7.003 7.003 0 0 0 8 15a7.002 7.002 0 0 0 6.53-4.47z"/>
+                                    </svg> 
+                                }
+                            </div>
+                        </div>  
+                        <div className="col d-block d-md-none">
+                            <div className={this.state.iconCol + " icozoom smallnav"}
+                                onClick={() => {this.showhome(); setInterval(() => {
+                                    this.hidehome();
+                                }, 1000);}}>
+                                <NavLink className="nonedec" to="/home">
+                                    <svg width="100%" height="auto" viewBox="0 0 16 16" class="bi bi-hexagon-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                                        <path fill-rule="evenodd" d="M8.5.134a1 1 0 0 0-1 0l-6 3.577a1 1 0 0 0-.5.866v6.846a1 1 0 0 0 .5.866l6 3.577a1 1 0 0 0 1 0l6-3.577a1 1 0 0 0 .5-.866V4.577a1 1 0 0 0-.5-.866L8.5.134z"/>
+                                    </svg>
+                                </NavLink>
+                            </div>
+                        </div>  
+                        <div className="col d-block d-md-none">
+                            <div className={this.state.iconCol + " icozoom smallnav"}
+                                onClick={() => {this.showabout(); setInterval(() => {
+                                    this.hideabout();
+                                }, 1000);}}>
+                                <NavLink className="nonedec" to="/about">
+                                    <svg class="bi bi-person-fill" width="100%" height="auto" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                                        <path fill-rule="evenodd" d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"/>
+                                    </svg>
+                                </NavLink>
+                            </div>
+                        </div>
+                        <div className="col d-block d-md-none">
+                            <div className={this.state.iconCol + " icozoom smallnav"}
+                                onClick={() => {this.showblog(); setInterval(() => {
+                                    this.hideblog();
+                                }, 1000);}}>
+                                <NavLink className="nonedec" to="/blog">
+                                    <svg width="100%" height="auto" viewBox="0 0 16 16" class="bi bi-file-text-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
                                         <path fill-rule="evenodd" d="M12 1H4a2 2 0 0 0-2 2v10a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V3a2 2 0 0 0-2-2zM5 4a.5.5 0 0 0 0 1h6a.5.5 0 0 0 0-1H5zm-.5 2.5A.5.5 0 0 1 5 6h6a.5.5 0 0 1 0 1H5a.5.5 0 0 1-.5-.5zM5 8a.5.5 0 0 0 0 1h6a.5.5 0 0 0 0-1H5zm0 2a.5.5 0 0 0 0 1h3a.5.5 0 0 0 0-1H5z"/>
                                     </svg>
                                 </NavLink>
@@ -116,8 +171,8 @@ export default class NavComp extends Component {
                         </div>
                     </div>
                     <div className="row justify-content-center align-items-center">
-                        <div className="col">
-                          {this.state.labels.changetheme ? <p className="jostfont textvis"> <br/> Change Theme</p> : <p className="jostfont texthid"> <br/> Change Theme</p>}
+                        <div className="col my-2">
+                          {this.state.labels.changetheme ? <p className="jostfont textvis"> <br/> Theme</p> : <p className="jostfont texthid"> <br/> Theme</p>}
                        </div>
                        <div className="col">
                           {this.state.labels.home ? <p className="jostfont textvis"> <br/> Home </p> : <p className="jostfont texthid"> <br/> Home </p>}
