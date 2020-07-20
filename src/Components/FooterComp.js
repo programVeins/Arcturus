9a826e8d83c458c609679794bfa0eaa4733b9020
Sabesh Bharathi <sabesh5601@gmail.com>
2020-07-21 01:23:22 +0530

    Dev with <3

--- src/Components/FooterComp.js
+++ src/Components/FooterComp.js
@@ -15,6 +15,15 @@ export default class FooterComp extends Component {
                     </div>
                 </div>
                 <p className="jostfont text-8">Sabesh Bharathi © 2020</p>
+                <span className="jostfont text-9">
+                    Developed with  
+                </span>
+                <span className="heart">
+                    <svg width="0.8em" height="0.8em" viewBox="0 0 16 16" class="bi bi-heart-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                        <path fill-rule="evenodd" d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z"/>
+                    </svg>
+                </span>
+                
                 <br/><br/>
             </div>
         )
