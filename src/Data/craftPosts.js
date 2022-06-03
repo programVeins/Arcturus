0211432f98bcd8c783cc29e8495615de1ef531d8
Sabesh Bharathi <sabesh@rocketlane.com>
2022-06-03 19:43:10 +0530

    Add typesense blog items

--- src/Data/craftPosts.js
+++ src/Data/craftPosts.js
@@ -39,6 +39,22 @@ const craftPosts = [
         desc: 'I have finally begun to learn CSS *properly* ! Here\'s the resources I\'ve been using to learn CSS, it basically comprises of a tutorial from web.dev combined with the CSS Podcast.',
         date: '2021-05-19T15:00+05:30'
     },
+    {
+        id: 5,
+        title: 'Full text search in MySQL',
+        thumbnail: 'assets/images/craftthumbs/mysql.png',
+        link: 'https://typesense.org/learn/full-text-search-mysql/',
+        desc: 'While MySQL is not a fully featured text search engine, it has enough tricks up its sleeve to be useful for implementing basic search features in your application. Let’s take a quick walk through.',
+        date: '2021-06-21T15:00+05:30'
+    },
+    {
+        id: 6,
+        title: 'Fuzzy Search with Javascript',
+        thumbnail: 'assets/images/craftthumbs/fuse.png',
+        link: 'https://typesense.org/learn/fuzzy-search-javascript/',
+        desc: 'Fuzzy Search refers to the process of approximately searching for a given search query. It may also be called as a “typo tolerant search”. Let\'s see how we can implement fuzzy search using JS',
+        date: '2021-06-10T15:00+05:30'
+    },
 
 ]
 
